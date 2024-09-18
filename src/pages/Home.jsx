40e9eb6801dc452d49853e26a30fc57cfec6d4df
Rifan <rifan@example.com>
2024-09-18 19:57:20 +0700

    meubah style pada home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,21 +6,16 @@ import FavoriteProduct from "../components/FavoriteProduct";
 import OverView from "../ui/OverView";
 import SpesialEdition from "../components/SpesialEdition";
 import FAQSection from "../components/FAQSection";
+
 export default function Home() {
   return (
-    <div className="bg-gradient-to-br from-gray-800 via-gray-900 to-black">
+    <div className="bg-gradient-to-br from-gray-800 via-gray-900 to-black overflow-x-hidden">
       <Header />
-
       <HeroPages />
-
       <FavoriteProduct />
-
       <CategoriesProducts />
-
       <OverView />
-
       <SpesialEdition />
-
       <FAQSection />
     </div>
   );
