602313f0e498ba5e613ab105e81c5ffeaf924c17
Rifan <rifan@example.com>
2024-09-19 18:19:00 +0700

    fix eror pada console log

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Update the import
 import App from "./App";
 import { CartProvider } from "./contexts/CartContext";
 import "./index.css";
 
-ReactDOM.render(
+// Create a root using createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the App wrapped in CartProvider
+root.render(
   <CartProvider>
     <App />
-  </CartProvider>,
-  document.getElementById("root")
+  </CartProvider>
 );
