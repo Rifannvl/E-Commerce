ce859033bc7b9d0c3ca981540d9fc1b0bbfffd27
Rifan <rifan@example.com>
2024-09-26 19:54:21 +0700

    memperbaiki header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,57 +16,59 @@ export default function Header() {
 
   const currentPage = location.pathname === "/";
   return (
-    <header className="fixed top-0  w-full z-50 glass backdrop-brightness-50 text-white container mx-auto py-4 items-center">
-      <div className="grid grid-cols-3 items-center gap-4 ">
-        {/* Logo */}
-        <Link to="/">
-          <h1 className="lg:text-3xl text-md font-bold  w-full">Rivnl</h1>
-        </Link>
-
-        <Link
-          to="/"
-          className={` ${
-            currentPage ? "text-yellow-400" : "text-white"
-          } font-semibold items-center flex justify-center `}
-        >
-          Home
-        </Link>
-
-        <div className="flex items-center gap-4 relative text-xl justify-end">
-          {/* icon for all product */}
-          <Link to="/products">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 lg:h-8 lg:w-8"
-              viewBox="0 0 1024 1024"
-            >
-              <path
-                fill="currentColor"
-                fillRule="evenodd"
-                d="M464 144c8.837 0 16 7.163 16 16v304c0 8.836-7.163 16-16 16H160c-8.837 0-16-7.164-16-16V160c0-8.837 7.163-16 16-16zm-52 68H212v200h200zm493.333 87.686c6.248 6.248 6.248 16.379 0 22.627l-181.02 181.02c-6.248 6.248-16.378 6.248-22.627 0l-181.019-181.02c-6.248-6.248-6.248-16.379 0-22.627l181.02-181.02c6.248-6.248 16.378-6.248 22.627 0zm-84.853 11.313L713 203.52L605.52 311L713 418.48zM464 544c8.837 0 16 7.164 16 16v304c0 8.837-7.163 16-16 16H160c-8.837 0-16-7.163-16-16V560c0-8.836 7.163-16 16-16zm-52 68H212v200h200zm452-68c8.837 0 16 7.164 16 16v304c0 8.837-7.163 16-16 16H560c-8.837 0-16-7.163-16-16V560c0-8.836 7.163-16 16-16zm-52 68H612v200h200z"
-              />
-            </svg>
+    <div className="fixed glass backdrop-brightness-50 top-0 left-0 right-0 w-full z-50">
+      <header className="   text-white container mx-auto  py-4 items-center">
+        <div className="grid grid-cols-3 items-center gap-4 ">
+          {/* Logo */}
+          <Link to="/">
+            <h1 className="lg:text-3xl text-md font-bold  w-full">Rivnl</h1>
           </Link>
-          {/* Cart Icon */}
-          <Link to="/cart" className="relative">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 lg:h-8 lg:w-8"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M17 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2M1 2h3.27l.94 2H20a1 1 0 0 1 1 1c0 .17-.05.34-.12.5l-3.58 6.47c-.34.61-1 1.03-1.75 1.03H8.1l-.9 1.63l-.03.12a.25.25 0 0 0 .25.25H19v2H7a2 2 0 0 1-2-2c0-.35.09-.68.24-.96l1.36-2.45L3 4H1zm6 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2m9-7l2.78-5H6.14l2.36 5z"
-              />
-            </svg>
-            {totalItems > 0 && (
-              <div className="absolute top-0 right-0 -translate-x-1/2 translate-y-1/2 bg-red-500 text-white text-xs font-bold rounded-full h-6 w-6 flex items-center justify-center">
-                {displayItems}
-              </div>
-            )}
+
+          <Link
+            to="/"
+            className={` ${
+              currentPage ? "text-yellow-400" : "text-white"
+            } font-semibold items-center flex justify-center `}
+          >
+            Home
           </Link>
+
+          <div className="flex items-center gap-4 relative text-xl justify-end">
+            {/* icon for all product */}
+            <Link to="/products">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 lg:h-8 lg:w-8"
+                viewBox="0 0 1024 1024"
+              >
+                <path
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  d="M464 144c8.837 0 16 7.163 16 16v304c0 8.836-7.163 16-16 16H160c-8.837 0-16-7.164-16-16V160c0-8.837 7.163-16 16-16zm-52 68H212v200h200zm493.333 87.686c6.248 6.248 6.248 16.379 0 22.627l-181.02 181.02c-6.248 6.248-16.378 6.248-22.627 0l-181.019-181.02c-6.248-6.248-6.248-16.379 0-22.627l181.02-181.02c6.248-6.248 16.378-6.248 22.627 0zm-84.853 11.313L713 203.52L605.52 311L713 418.48zM464 544c8.837 0 16 7.164 16 16v304c0 8.837-7.163 16-16 16H160c-8.837 0-16-7.163-16-16V560c0-8.836 7.163-16 16-16zm-52 68H212v200h200zm452-68c8.837 0 16 7.164 16 16v304c0 8.837-7.163 16-16 16H560c-8.837 0-16-7.163-16-16V560c0-8.836 7.163-16 16-16zm-52 68H612v200h200z"
+                />
+              </svg>
+            </Link>
+            {/* Cart Icon */}
+            <Link to="/cart" className="relative">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6 lg:h-8 lg:w-8"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M17 18a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2M1 2h3.27l.94 2H20a1 1 0 0 1 1 1c0 .17-.05.34-.12.5l-3.58 6.47c-.34.61-1 1.03-1.75 1.03H8.1l-.9 1.63l-.03.12a.25.25 0 0 0 .25.25H19v2H7a2 2 0 0 1-2-2c0-.35.09-.68.24-.96l1.36-2.45L3 4H1zm6 16a2 2 0 0 1 2 2a2 2 0 0 1-2 2a2 2 0 0 1-2-2c0-1.11.89-2 2-2m9-7l2.78-5H6.14l2.36 5z"
+                />
+              </svg>
+              {totalItems > 0 && (
+                <div className="absolute top-0 right-0 -translate-x-1/2 translate-y-1/2 bg-red-500 text-white text-xs font-bold rounded-full h-6 w-6 flex items-center justify-center">
+                  {displayItems}
+                </div>
+              )}
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </div>
   );
 }
