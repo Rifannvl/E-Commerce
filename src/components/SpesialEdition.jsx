ea294f99cc6c9286b647fd92ee9598e39cf1869d
Rifan <rifan@example.com>
2024-09-26 19:46:02 +0700

    memperbaiki tampilan atau stylenya

--- src/components/SpesialEdition.jsx
+++ src/components/SpesialEdition.jsx
@@ -5,51 +5,10 @@ import Aos from "aos";
 export default function SpesialEdition() {
   Aos.init();
   return (
-    <div className="container ">
-      {/* <section className="rounded-xl 2xl:py-24 2xl:bg-white container mx-auto my-10 ">
-        <div className="container mx-auto w-11/12 px-4  overflow-hidden glass backdrop-brightness-50  sm:px-6 lg:px-8 rounded-xl ">
-          <div className="py-10 sm:py-16 lg:py-24 2xl:pl-24">
-            <div className="grid items-center grid-cols-1 gap-y-12 lg:grid-cols-2 lg:gap-x-8 2xl:gap-x-20">
-              <div>
-                <h2 className="text-3xl font-bold leading-tight text-white sm:text-4xl lg:text-5xl lg:leading-tight">
-                  Spesial Edition
-                </h2>
-                <p className="mt-4 text-base text-gray-50">
-                  Amet minim mollit non deserunt ullamco est sit aliqua dolor do
-                  amet sint. Velit officia consequat duis enim velit mollit.
-                  Exercitation veniam consequat sunt nostrud amet.
-                </p>
-                <h5 className="mt-8 text-white text-2xl">
-                  Discount 20%, Limited Edition Only. Use Code: NEWOFF20%{" "}
-                </h5>
-
-                <div className="flex flex-row items-center mt-8 space-x-4 lg:mt-12">
-                  <a href="#" title="" className="flex" role="button"></a>
-                  <a href="#" title="" className="flex" role="button"></a>
-                </div>
-              </div>
-              <div className="relative px-12">
-                <svg
-                  className="absolute inset-x-0 bottom-0 left-1/2 -translate-x-1/2 -mb-48 lg:-mb-72 text-yellow-300 w-[460px] h-[460px] sm:w-[600px] sm:h-[600px]"
-                  fill="currentColor"
-                  viewBox="0 0 8 8"
-                >
-                  <circle cx={4} cy={4} r={3} />
-                </svg>
-                <img
-                  className="relative w-full max-w-xs mx-auto -mb-60 lg:-mb-64"
-                  src="https://picsum.photos/id/237/460/460"
-                  alt=""
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section> */}
-
+    <div className="container mx-auto ">
       {/* Table untuk experience */}
       <section className="py-10  sm:py-16 lg:py-24">
-        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="">
           {/* <div className="text-center">
             <h4 className="text-xl font-medium text-white">
               Tabel angka menunjukkan kerja keras yang telah kami lakukan dalam
